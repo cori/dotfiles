aede74531ef446120f5094ef8a36ae04b9d6fe8c
cori schlegel <cori@glitch.com>
2020-06-25 08:06:11 -0500

    open github urls in Chrome (for toggl timer usage)

--- .finicky.js
+++ .finicky.js
@@ -8,6 +8,13 @@ module.exports = {
         /.*\.google.com$/ // match all google.com subdomains
       ]),
       browser: "Google Chrome"
+    },
+    {
+      // Open github.com urls in Google Chrome
+      match: finicky.matchHostnames([
+        "github.com" // match google.com domain as string (to make regular expression less complicated)
+      ]),
+      browser: "Google Chrome"
     }
   ]
 };

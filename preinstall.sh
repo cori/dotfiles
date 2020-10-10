a02a2bb297dd15eceec84d24e650f33f3ea3c94f
cori schlegel <cori@glitch.com>
2020-10-10 05:33:59 -0500

    add preinstall script for oh-my-zsh

---
+++ preinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if Homebrew is installed
+if [ ! -f "`which brew`" ]; then
+  echo 'Installing homebrew'
+  cd /usr/local
+  mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+else
+  echo 'Updating homebrew'
+  brew update
+fi
+
+# Check if oh-my-zsh is installed
+OMZDIR="$HOME/.oh-my-zsh"
+if [ ! -d "$OMZDIR" ]; then
+  echo 'Installing oh-my-zsh'
+  /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo 'Updating oh-my-zsh'
+  upgrade_oh_my_zsh
+fi
+
+# Change default shell
+if [! $0 = "-zsh"]; then
+  echo 'Changing default shell to zsh'
+  chsh -s /bin/zsh
+else
+  echo 'Already using zsh'
+fi

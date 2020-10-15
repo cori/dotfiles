c06ea344bcded6cea44972f6fea2937dc8fd9fad
cori schlegel <cori@glitch.com>
2020-10-15 07:14:51 -0500

    clean up finicky

--- .finicky.js
+++ .finicky.js
@@ -5,14 +5,22 @@ module.exports = {
       // Open google.com and *.google.com urls in Google Chrome
       match: finicky.matchHostnames([
         "google.com", // match google.com domain as string (to make regular expression less complicated)
-        /.*\.google.com$/ // match all google.com subdomains
+        /.*\.google\.com$/ // match all google.com subdomains
       ]),
       browser: "Google Chrome"
     },
     {
       // Open github.com urls in Google Chrome
       match: finicky.matchHostnames([
-        "github.com" // match google.com domain as string (to make regular expression less complicated)
+        "github.com"
+      ]),
+      browser: "Google Chrome"
+    },
+    {
+      // Open clubhouse.com urls in Google Chrome
+      match: finicky.matchHostnames([
+        "clubhouse.io",
+        /.*\.clubhouse.io/
       ]),
       browser: "Google Chrome"
     }

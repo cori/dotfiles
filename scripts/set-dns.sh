e2d3430df74957d56a10c1ea9a11c1e094e49f10
cori schlegel <cori@glitch.com>
2021-03-30 10:29:59 -0500

    also set wired connection

--- scripts/set-dns.sh
+++ scripts/set-dns.sh
@@ -1,1 +1,2 @@
 networksetup -setdnsservers Wi-Fi "$1" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
+networksetup -setdnsservers "USB 10/100/1000 LAN" "$1" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;

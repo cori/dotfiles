11e5686f727fcabf5f335ef363594401146cc7fa
cori <cori@glitch.com>
2021-09-02 08:52:29 -0500

    make set-dns handle the Glitch-y stuff too

--- scripts/set-dns.sh
+++ scripts/set-dns.sh
@@ -1,2 +1,5 @@
-networksetup -setdnsservers Wi-Fi "$1" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
-networksetup -setdnsservers "USB 10/100/1000 LAN" "$1" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
+~/code/glitchdotcom/glitch/sh/dns/update-ip.sh cori
+ip=$(cat /Users/cori/code/glitchdotcom/glitch/sh/.backend-cori.ip)
+
+networksetup -setdnsservers Wi-Fi "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
+networksetup -setdnsservers "USB 10/100/1000 LAN" "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;

c5b9919bdd9a1f293085fb62b949d0e142be0755
cori <cori@glitch.com>
2022-07-11 09:48:17 -0500

    adjust profiles

--- .finicky.js
+++ .finicky.js
@@ -1,4 +1,4 @@
-//  on ShadowDepository@Fastly, Profile 2 is glitch.com and Profile 1 is "Work" (Fastly)
+//  on ShadowDepository@Fastly@2.0, glitch is "Profile 1" and Fastly is "Default"
 module.exports = {
   defaultBrowser: "Safari",
   options: {
@@ -14,245 +14,245 @@ module.exports = {
     }
   }],
   handlers: [{
-      // Open google.com and *.google.com urls in Google Chrome
-      match: finicky.matchHostnames([
-        "google.com", // match google.com domain as string (to make regular expression less complicated)
-        /.*\.google\.com$/ // match all google.com subdomains
-      ]),
-      browser: "Google Chrome"
-    },
-    {
-      // Open github.com urls in Google Chrome
-      match: finicky.matchHostnames([
-        "github.com"
-      ]),
-      browser: "Google Chrome"
-    },
-    {
-      // Open clubhouse.io urls in Google Chrome
-      match: finicky.matchHostnames([
-        "clubhouse.io",
-        /.*\.clubhouse.io/
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open clubhouse.io urls in Google Chrome
-      match: finicky.matchHostnames([
-        "shortcut.com",
-        /.*\.shortcut.com/
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open honeycomb urls in Google Chrome
-      match: finicky.matchHostnames([
-        "honeycomb.io",
-        /.*\.honeycomb.io/
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open AWS urls in Google Chrome
-      match: finicky.matchHostnames([
-        "aws.amazon.com",
-        /.*\.aws.amazon.com/,
-        /.*\.amazonaws.com/
-      ]),
-      browser: "Google Chrome"
-    },
-    {
-      // Open trello.com urls in Google Chrome
-      match: finicky.matchHostnames([
-        "trello.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open circleci.com urls in Google Chrome
-      match: finicky.matchHostnames([
-        "circleci.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Lattice in Google Chrome
-      match: finicky.matchHostnames([
-        "glitch.latticehq.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Glitch's pagerduty in Google Chrome
-      match: finicky.matchHostnames([
-        "fogcreek.pagerduty.com",
-        "app.pagerduty.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open glitch's dev watcher in Google Chrome
-      match: "http://localhost:1234",
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Grafana urls in Google Chrome
-      match: finicky.matchHostnames([
-        "grafana.glitch.com",
-        "grafana.staging.glitch.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open kibana urls in Google Chrome
-      match: finicky.matchHostnames([
-        "kibana.glitch.com",
-        "kibana.staging.glitch.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open metabase urls in Google Chrome
-      match: finicky.matchHostnames([
-        "metabase.glitch.com",
-        "metabase.staging.glitch.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Glitch HappyFox urls in Google Chrome
-      match: finicky.matchHostnames([
-        "help.glitch.com",
-        "glitch.happyfox.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Glitch in Chrome
-      match: finicky.matchHostnames([
-        "glitch.com",
-        "glitch.me"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Notion urls in Google Chrome
-      match: finicky.matchHostnames([
-        "notion.so",
-        "www.notion.so"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    {
-      // Open Sentry urls in Google Chrome
-      match: finicky.matchHostnames([
-        "sentry.io",
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 2"
-      }
-    },
-    //  Fastly
-    {
-      // Open Fastlassian urls in Google Chrome
-      match: finicky.matchHostnames([
-        "fastly.atlassian.net",
-        "confluence.corp.fastly.com"
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 1"
-      }
-    },
-    {
-      // Open Fastly Okta urls in Google Chrome
-      match: finicky.matchHostnames([
-        "fastly.okta.com",
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 1"
-      }
-    },
-    {
-      // Open Fastly zoom urls in Google Chrome
-      match: finicky.matchHostnames([
-        "fastly.zoom.us",
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 1"
-      }
-    },
-    {
-      // not sure exactly where these cme from, but open them in Google Chrome
-      match: finicky.matchHostnames([
-        /.*\.visualforce\.com/,
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 1"
-      }
-    },
-    {
-      // not sure exactly where these cme from, but open them in Google Chrome
-      match: finicky.matchHostnames([
-        /.*\.secretcdn\.net/,
-      ]),
-      browser: {
-        name: "Google Chrome",
-        profile: "Profile 1"
-      }
-    },
-    {
-      //	open everything from NewsExplorer in STP (since it opens Safari directly)
-      match: ({
-        opener
-      }) => opener.bundleId = "betamagic.News-Explorer",
-      browser: "Safari"
-    },
+    // Open google.com and *.google.com urls in Google Chrome
+    match: finicky.matchHostnames([
+      "google.com", // match google.com domain as string (to make regular expression less complicated)
+      /.*\.google\.com$/ // match all google.com subdomains
+    ]),
+    browser: "Google Chrome"
+  },
+  {
+    // Open github.com urls in Google Chrome
+    match: finicky.matchHostnames([
+      "github.com"
+    ]),
+    browser: "Google Chrome"
+  },
+  {
+    // Open clubhouse.io urls in Google Chrome
+    match: finicky.matchHostnames([
+      "clubhouse.io",
+      /.*\.clubhouse.io/
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open clubhouse.io urls in Google Chrome
+    match: finicky.matchHostnames([
+      "shortcut.com",
+      /.*\.shortcut.com/
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open honeycomb urls in Google Chrome
+    match: finicky.matchHostnames([
+      "honeycomb.io",
+      /.*\.honeycomb.io/
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open AWS urls in Google Chrome
+    match: finicky.matchHostnames([
+      "aws.amazon.com",
+      /.*\.aws.amazon.com/,
+      /.*\.amazonaws.com/
+    ]),
+    browser: "Google Chrome"
+  },
+  {
+    // Open trello.com urls in Google Chrome
+    match: finicky.matchHostnames([
+      "trello.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open circleci.com urls in Google Chrome
+    match: finicky.matchHostnames([
+      "circleci.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Lattice in Google Chrome
+    match: finicky.matchHostnames([
+      "glitch.latticehq.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Glitch's pagerduty in Google Chrome
+    match: finicky.matchHostnames([
+      "fogcreek.pagerduty.com",
+      "app.pagerduty.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open glitch's dev watcher in Google Chrome
+    match: "http://localhost:1234",
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Grafana urls in Google Chrome
+    match: finicky.matchHostnames([
+      "grafana.glitch.com",
+      "grafana.staging.glitch.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open kibana urls in Google Chrome
+    match: finicky.matchHostnames([
+      "kibana.glitch.com",
+      "kibana.staging.glitch.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open metabase urls in Google Chrome
+    match: finicky.matchHostnames([
+      "metabase.glitch.com",
+      "metabase.staging.glitch.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Glitch HappyFox urls in Google Chrome
+    match: finicky.matchHostnames([
+      "help.glitch.com",
+      "glitch.happyfox.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Glitch in Chrome
+    match: finicky.matchHostnames([
+      "glitch.com",
+      "glitch.me"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Notion urls in Google Chrome
+    match: finicky.matchHostnames([
+      "notion.so",
+      "www.notion.so"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  {
+    // Open Sentry urls in Google Chrome
+    match: finicky.matchHostnames([
+      "sentry.io",
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Profile 1"
+    }
+  },
+  //  Fastly
+  {
+    // Open Fastlassian urls in Google Chrome
+    match: finicky.matchHostnames([
+      "fastly.atlassian.net",
+      "confluence.corp.fastly.com"
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Default"
+    }
+  },
+  {
+    // Open Fastly Okta urls in Google Chrome
+    match: finicky.matchHostnames([
+      "fastly.okta.com",
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Default"
+    }
+  },
+  {
+    // Open Fastly zoom urls in Google Chrome
+    match: finicky.matchHostnames([
+      "fastly.zoom.us",
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Default"
+    }
+  },
+  {
+    // not sure exactly where these cme from, but open them in Google Chrome
+    match: finicky.matchHostnames([
+      /.*\.visualforce\.com/,
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Default"
+    }
+  },
+  {
+    // not sure exactly where these cme from, but open them in Google Chrome
+    match: finicky.matchHostnames([
+      /.*\.secretcdn\.net/,
+    ]),
+    browser: {
+      name: "Google Chrome",
+      profile: "Default"
+    }
+  },
+  {
+    //	open everything from NewsExplorer in STP (since it opens Safari directly)
+    match: ({
+      opener
+    }) => opener.bundleId = "betamagic.News-Explorer",
+    browser: "Safari"
+  },
   ]
 };

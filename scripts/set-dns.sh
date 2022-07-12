df1729ec0f62f2c103bad6427c520671d59a1821
cori <cori@glitch.com>
2022-07-11 23:29:28 -0500

    update LAN name

--- scripts/set-dns.sh
+++ scripts/set-dns.sh
@@ -2,4 +2,4 @@
 ip=$(cat /Users/cori/code/glitchdotcom/glitch/sh/.backend-cori.ip)
 
 networksetup -setdnsservers Wi-Fi "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
-networksetup -setdnsservers "USB 10/100/1000 LAN" "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
+networksetup -setdnsservers "AX88179A" "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;

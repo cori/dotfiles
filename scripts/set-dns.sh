81bcc6eaf5f06292ac163f2bbe68c3654c5eff89
cori <cori@glitch.com>
2023-08-04 07:59:53 -0500

    set dns for wired network

--- scripts/set-dns.sh
+++ scripts/set-dns.sh
@@ -2,4 +2,4 @@
 ip=$(cat /Users/cori/code/glitchdotcom/glitch/sh/.backend-cori.ip)
 
 networksetup -setdnsservers Wi-Fi "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
-networksetup -setdnsservers "Thunderbolt Ethernet Slot 0 2" "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
+networksetup -setdnsservers "Thunderbolt Ethernet Slot 0" "$ip" 1.1.1.1 8.8.8.8 192.168.1.1 && sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder;
